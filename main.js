faa6230eb31f28bf2139ed66a4cdea30eb57276b
Michelle Meyer <michelle.meyer@stud.hslu.ch>
2019-10-18 21:06:07 +0200

    The real navbar, cookie

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 $(document).ready(function () {
-    document.cookie = "GoeselCookie=" + Date.now();
+    document.cookie = "LastVisit=" + Date.now().toString();
 
     $(".nav-link").click(function () {
         $(".nav-link").removeClass("active");
         $(this).addClass("active");
-    })
-
-
+    });
 });
+
+function openHatch(material) {
+    $("#disposal-feedback").text("Die Abfallluke wird geöffnet für die Entsorgung von " + material);
+    setTimeout(() => {
+        $("#disposal-feedback").text("");
+    }, 6000);
+}
