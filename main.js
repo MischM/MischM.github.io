5290cb13b699cb7ee1c15b627e1c51096fff8a1b
Michelle Meyer <michelle.meyer@stud.hslu.ch>
2019-10-21 22:34:24 +0200

    ajax

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    getAmount();
     document.cookie = "LastVisit=" + Date.now().toString();
 
     $(".nav-link").click(function () {
@@ -16,4 +17,33 @@ function openHatch(material) {
 
 function unlock() {
     $("#login-mask").hide();
+}
+function getAmount() {
+    let data = {
+        'jsonrpc': '2.0',
+        'method': 'generateIntegers',
+        'params': {
+            'apiKey': RANDOM_API_KEY,
+            'n': 5,
+            'min': 0,
+            'max': 7,
+            'replacement': true,
+        },
+        'id': 4321
+    }
+
+    $.ajax({
+        url: 'https://api.random.org/json-rpc/1/invoke',
+        type: "POST",
+        data: JSON.stringify(data), // stringify data object
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (response) {
+            $('#rubish-amount').append(JSON.stringify(response.result.random.data[0] + "kg"));
+            $('#paper-amount').append(JSON.stringify(response.result.random.data[1] + "kg"));
+            $('#glass-amount').append(JSON.stringify(response.result.random.data[2] + "kg"));
+            $('#alu-amount').append(JSON.stringify(response.result.random.data[3] + "kg"));
+            console.log(response.result.random.data);
+        }
+    });
 }
